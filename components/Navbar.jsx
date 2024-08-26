c06b81e17b9f531b68e489495d783cf4fecf7110
0bese <134105909+0bese@users.noreply.github.com>
2024-08-26 16:57:08 -0400

    toDo: Stripe

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,9 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-16 px-5 shadow-md">
-      <Image src={"/logo.svg"} height={30} width={120} alt="logo" />
+      <Link href={"/"}>
+        <Image src={"/logo.svg"} height={30} width={120} alt="logo" />
+      </Link>
       <div className="flex gap-4 items-center justify-center">
         <Link
           className="border-2 py-2 px-4 rounded-3xl hover:cursor-pointer hover:shadow-2xl"
